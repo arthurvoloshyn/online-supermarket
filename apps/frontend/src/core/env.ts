c696e4dd8973bacfb83ee1f924eff22ee569cd86
arturvoloshyn <arturvoloshyn@gmail.com>
2024-01-29 23:41:58 +0200

    fix(frontend): add REACT_APP_ prefix to API_BASE_URL env

--- apps/frontend/src/core/env.ts
+++ apps/frontend/src/core/env.ts
@@ -1,4 +1,4 @@
 export const __DEV__: boolean =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-export const API_BASE_URL: string = process.env.API_BASE_URL || "http://127.0.0.1:7000/";
+export const API_BASE_URL: string = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:7000/";
